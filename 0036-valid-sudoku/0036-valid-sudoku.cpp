a4f763543dccf720f7332a93c10dd0ac321c87d8
Rudra Pratap Singh Rathore <152763842+rudrarathore169@users.noreply.github.com>
2025-07-14 15:30:01 +0530

    Time: 5 ms (43.85%), Space: 25.8 MB (20.19%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+    unordered_set<string> seen;
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            char num = board[i][j];
+            if (num != '.') {
+                string row = string(1, num) + " in row " + to_string(i);
+                string col = string(1, num) + " in col " + to_string(j);
+                string box = string(1, num) + " in box " + to_string(i/3) + "-" + to_string(j/3);
+                if (!seen.insert(row).second || !seen.insert(col).second || !seen.insert(box).second)
+                    return false;
+            }
+        }
+    }
+    return true;
+}
+
+};
