e12f3e9006b7df89018ee971753e66d0c44d8cdc
Rudra Pratap Singh Rathore <152763842+rudrarathore169@users.noreply.github.com>
2026-07-15 11:51:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int odd = 1;
-        for(int i=1;i<n;i++){
-            odd+=i*2+1;
-        }
-        int even=0;
-        for(int i=1;i<=n;i++){
-            even+=i*2;
-        }
-        int ans = 1;
-        for(int i=1;i<even&&i<odd;i++){
-            if(even%i==0&&odd%i==0){
-                ans=max(ans,i);
-            }
-        }
-        return ans;
+        return n;
     }
 };
